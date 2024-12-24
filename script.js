f0f007dfce74ec99bb7984efa4ec74e2e8f0d6fc
ShrutiGaud3 <shrutigaud3@gmail.com>
2024-12-25 00:26:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,77 @@
+const box =document.querySelector('box')
+
+
+const diceOne='<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32zM224 224a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>'
+
+const diceTwo='<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M0 96C0 60.7 28.7 32 64 32l320 0c35.3 0 64 28.7 64 64l0 320c0 35.3-28.7 64-64 64L64 480c-35.3 0-64-28.7-64-64L0 96zM352 352a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zM128 192a32 32 0 1 0 0-64 32 32 0 1 0 0 64z"/></svg>'
+
+const diceThree='<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32zm64 96a32 32 0 1 1 0 64 32 32 0 1 1 0-64zm64 128a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zm128 64a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>'
+
+const diceFour='<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M0 96C0 60.7 28.7 32 64 32l320 0c35.3 0 64 28.7 64 64l0 320c0 35.3-28.7 64-64 64L64 480c-35.3 0-64-28.7-64-64L0 96zm160 64a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zM128 384a32 32 0 1 0 0-64 32 32 0 1 0 0 64zM352 160a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zM320 384a32 32 0 1 0 0-64 32 32 0 1 0 0 64z"/></svg>'
+
+const diceFive='<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32zm64 96a32 32 0 1 1 0 64 32 32 0 1 1 0-64zM96 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zM224 224a32 32 0 1 1 0 64 32 32 0 1 1 0-64zm64-64a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zm32 160a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>'
+
+const diceSix='<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M0 96C0 60.7 28.7 32 64 32l320 0c35.3 0 64 28.7 64 64l0 320c0 35.3-28.7 64-64 64L64 480c-35.3 0-64-28.7-64-64L0 96zm160 64a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zM128 288a32 32 0 1 0 0-64 32 32 0 1 0 0 64zm32 64a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zM320 192a32 32 0 1 0 0-64 32 32 0 1 0 0 64zm32 64a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zM320 384a32 32 0 1 0 0-64 32 32 0 1 0 0 64z"/></svg>'
+
+
+const randomDice=[diceOne,diceTwo, diceThree, diceFour, diceFive, diceSix]
+
+
+const randomQuote=[{
+     quote:' "Love the life you live. Live the life you love. " ',
+     author:' -Bob Marley '
+
+},
+{
+   quote:' "The beauty of life is in small details, noit in big events." ',
+   author:'-Jim Jarmusch'
+},
+{
+    quote:' "Problem disapper when we are willing to become flexible."',
+    author:' -Roxana Jones'
+},
+{
+    quote:'"If you can change your mind, you can change your life."',
+    author:'-William James'
+},{
+    quote:'"All it takes is one idea to solve an impossible problem."',
+    author:' -Robert H. Schuller'
+},{
+    quote:'"Life is a balance between holding on and letting go."',
+    author:'-Rumi'
+}
+]
+
+// console.log(randomQuote.quote)
+
+const diceBtn= document.querySelector('#dice')
+const heading=document.querySelector('h1')
+const para= document.querySelector('p')
+
+function randomDiceButton(){
+
+    // rotate button
+    diceBtn.style.transform='rotate(150deg)' 
+
+
+   const random= Math.floor(Math.random()*randomDice.length)
+   diceBtn.innerHTML=randomDice[random]
+   
+
+
+   // Generate a random quote index
+    const indexQuotes =Math.floor(Math.random()*randomQuote.length)
+    const selectedQuotes=randomQuote[indexQuotes]
+
+  // Set the text for the heading and paragraph
+    heading.innerText=selectedQuotes.quote
+    para.innerText=selectedQuotes.author
+
+
+    setTimeout(()=>{
+        diceBtn.style.transform='rotate(0deg)'
+    },200)
+}
+  
+diceBtn.addEventListener('click',randomDiceButton)
+
